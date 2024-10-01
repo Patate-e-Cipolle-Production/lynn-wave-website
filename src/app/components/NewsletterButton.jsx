67cef9231c37d765d63514cbdbd3dcef28342360
AndrenucciAndrea <andrenucciandrea01@gmail.com>
2024-10-01 15:41:37 +0200

    Aggiunto contatti

--- src/app/components/NewsletterButton.jsx
+++ src/app/components/NewsletterButton.jsx
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion'
 const NewsletterButton = () => {
     return (
         <motion.button
-            whileTap={{ scale: 0.95, shadow: 0 }}
-            className="2xl:text-3xl text-xl flex items-center justify-center bg-white text-tertiary md:rounded-2xl rounded-xl 2xl:w-[520px] 2xl:h-[100px] md:w-[380px] md:h-[70px] w-full h-[100px] hover:bg-secondary shadow-default-card">
+            whileTap={{ scale: 0.95, boxShadow: "0px 1px 20px 1px rgba(0, 0, 0, 0.08)" }}
+            className="2xl:text-3xl transition-all text-xl flex items-center justify-center bg-white text-tertiary md:rounded-2xl rounded-xl 2xl:w-[520px] 2xl:h-[100px] md:w-[380px] md:h-[70px] w-full h-[100px] hover:bg-secondary shadow-default-card">
             Iscriviti alla newsletter
         </motion.button>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 "use client"
-import Image from "next/image";
 import { Card, Navbar, Canvas3D, NewsletterButton } from "./components";
-import { esp32 } from "./assets";
-import { motion } from "framer-motion";
-
 
 const Home = () => {
   return (
@@ -35,9 +31,16 @@ const Home = () => {
         </div>
       </section>
 
-      <div id='contacts' />
-      <section className="flex flex-row items-center justify-center w-full h-screen">
-        Caio
+      <section div id='contacts' className="flex flex-col md:flex-row items-center justify-center w-full pt-[80px] h-[780px] px-[80px]">
+        <div className="md:w-1/2 w-full h-full flex flex-col justify-start">
+          <h1 className="font-bold text-4xl text-tertiary">Contatti</h1>
+          <div>
+            {/* //TODO Map contatti */}
+          </div>
+        </div>
+        <div className="w-1/2 h-full flex flex-col justify-center">
+          {/* //TODO form */}
+        </div>
       </section>
     </div>
   );
